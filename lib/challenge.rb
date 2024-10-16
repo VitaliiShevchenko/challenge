3ef54d8d82165236477a1286f5126adebfcef193
VitaliiShevchenko <iksel@ukr.net>
2024-10-17 00:05:50 +0300

    Create Localout Class and modified Challenge class

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,24 +1,30 @@
 # frozen_string_literal: true
 
+require_relative '../lib/company'
+require_relative '../lib/user'
+require 'json'
+
+# The main Class which performs all processes of the task according to certain algorithm
 class Challenge
   attr_reader :companies, :users
 
   # load data from needy files
   def initialize
-    company_path = '../data/input/companies.json'
-    user_path = '../data/input/users.json'
+    company_path = 'data/input/companies.json'
+    user_path = 'data/input/users.json'
 
-    @companies = Company.new(company_path)
-    @users = User.new(user_path)
+    @companies = Company.loader(company_path)
+    @users = User.loader(user_path)
   end
 
   def run
-    puts "Welcome to Challenge Game"
+    puts 'Welcome to Challenge Game'
   end
+
 end
 
 # To run the challenge
-if __FILE__ == $0
+if __FILE__ == $PROGRAM_NAME
   challenge = Challenge.new
   challenge.run
 end
---
+++ lib/localout.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+## Class which performs:
+# Load (LO)
+# Calculation (CAL)
+# Output (OUT) necessary data
+class Localout
+  def initialize(companies, users)
+    @companies = companies
+    @users = users
+  end
+
+  # Calculation and store the data
+
+  # Output received data to the file
+
+
+end
