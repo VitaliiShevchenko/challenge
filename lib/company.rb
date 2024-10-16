4cf16aac0bb7eca79b3b1755f4e728af43b7225b
VitaliiShevchenko <iksel@ukr.net>
2024-10-16 22:07:31 +0300

    Modified Company class and create Rspec test for it

--- lib/company.rb
+++ lib/company.rb
@@ -1,12 +1,45 @@
 # frozen_string_literal: true
 
+# Class for acquisition data about Company and prevent errors by loading from the files
 class Company
-  attr_accessor  :id, :name, :top_up, :email_status
+  ATTRIBUTES = %w[id name top_up email_status].freeze
+  attr_accessor :id, :name, :top_up, :email_status
 
   def initialize(attributes)
     @id = attributes['id']
     @name = attributes['name']
     @top_up = attributes['top_up']
     @email_status = attributes['email_status']
+  end
+
+  class << self
+    def loader(file_path)
+      load_json(file_path)
+    end
+
+    private
+
+    def load_json(file_path)
+
+      data = JSON.parse(File.read(file_path))
+      validate_data(data)
+    rescue JSON::ParserError => e
+      puts "Error parsing JSON from #{file_path}: #{e.message}"
+      {}
+    rescue StandardError => e
+      puts "Error loading data from #{file_path}: #{e.message}"
+      {}
+
+    end
+
+    def validate_data(data)
+      # Specific validation logic for Company
+      raise "Expected a Hash but got #{data.class}" unless data.is_a?(Hash)
+
+      @missing_keys = ATTRIBUTES.reject { |key| data.key?(key) }
+      raise "Missing required keys: #{@missing_keys.join(', ')}" unless @missing_keys.empty?
+
+      data
+    end
   end
 end
---
+++ spec/company_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../lib/company'
+require 'json'
+
+RSpec.describe 'Company' do
+  let(:valid_data) { { 'id' => 1, 'name' => 'Blue Cat Inc.', 'top_up' => 71, 'email_status' => 'false' } }
+  let(:invalid_data) { { 'id' => 1, 'name' => 'Blue Cat Inc.' } } # Missing required keys
+  describe '.initialize' do
+    it 'correctly initializes attributes' do
+      company = Company.new(valid_data)
+      expect(company.id).to eq(1)
+      expect(company.name).to eq('Blue Cat Inc.')
+      expect(company.top_up).to eq(71)
+      expect(company.email_status).to eq('false')
+    end
+  end
+
+  describe '.loader' do
+    let(:file_path) { '../data/input/companies.json' }
+
+    before do
+      allow(File).to receive(:read).with(file_path).and_return(valid_data.to_json)
+    end
+
+    it 'loads valid JSON data' do
+      company_data = Company.loader(file_path)
+      expect(company_data['id']).to eq(1)
+      expect(company_data['name']).to eq('Blue Cat Inc.')
+    end
+
+    it 'handles JSON parsing errors' do
+      allow(File).to receive(:read).with(file_path).and_return('{invalid_json}')
+      expect { Company.loader(file_path) }.to output(/Error parsing JSON/).to_stdout
+    end
+
+    it 'handles missing required keys' do
+      allow(File).to receive(:read).with(file_path).and_return(invalid_data.to_json)
+      expect { Company.loader(file_path) }.to output("Error loading data from ../data/input/companies.json: Missing required keys: top_up, email_status\n").to_stdout
+    end
+  end
+end
